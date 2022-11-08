000a4871636cc503c34da95b37df7720ac163caf
Muhammad Haroon <m_haroon96@hotmail.com>
2022-11-07 16:34:51 -0800

    added desc

--- helpers.py
+++ helpers.py
@@ -1,11 +1,12 @@
 import re
 
 class Short:
-    def __init__(self, elem=None, url=None):
+    def __init__(self, elem=None, url=None, description=None):
         self.elem = elem
         self.url = url
         search = re.search(".*/(?P<user>@.+?)/video/(?P<videoId>[0-9]+)", url)
         self.shortId = f"{search.group('user')}/{search.group('videoId')}"
+        self.description = description
 
 class ShortUnavailableException(Exception):
     pass
